c50f6d7edf38206ed35f29cf239868b2a9038c2b
IvanMouses <mavsis@mail.ru>
2025-07-11 15:47:14 +0300

    summer courses updated

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -7,97 +7,12 @@ import { Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/bundle";
 
-const data = [
-  {
-    id: 1,
-    date: new Date("05-29-2025").toDateString(),
-  },
-  {
-    id: 2,
-    date: new Date("05-30-2025").toDateString(),
-  },
-  {
-    id: 3,
-    date: new Date("06-02-2025").toDateString(),
-  },
-  {
-    id: 4,
-    date: new Date("06-03-2025").toDateString(),
-  },
-  {
-    id: 5,
-    date: new Date("06-04-2025").toDateString(),
-  },
-  {
-    id: 6,
-    date: new Date("06-05-2025").toDateString(),
-  },
-  {
-    id: 7,
-    date: new Date("06-06-2025").toDateString(),
-  },
-  {
-    id: 8,
-    date: new Date("06-09-2025").toDateString(),
-  },
-  {
-    id: 9,
-    date: new Date("06-10-2025").toDateString(),
-  },
-  {
-    id: 10,
-    date: new Date("06-11-2025").toDateString(),
-  },
-  {
-    id: 11,
-    date: new Date("06-16-2025").toDateString(),
-  },
-  {
-    id: 12,
-    date: new Date("06-17-2025").toDateString(),
-  },
-  {
-    id: 13,
-    date: new Date("06-18-2025").toDateString(),
-  },
-  {
-    id: 14,
-    date: new Date("06-19-2025").toDateString(),
-  },
-  {
-    id: 15,
-    date: new Date("06-20-2025").toDateString(),
-  },
-  {
-    id: 16,
-    date: new Date("06-23-2025").toDateString(),
-  },
-  {
-    id: 17,
-    date: new Date("06-24-2025").toDateString(),
-  },
-  {
-    id: 18,
-    date: new Date("06-25-2025").toDateString(),
-  },
-  {
-    id: 19,
-    date: new Date("06-26-2025").toDateString(),
-  },
-  {
-    id: 20,
-    date: new Date("06-27-2025").toDateString(),
-  },
-  {
-    id: 21,
-    date: new Date("06-30-2025").toDateString(),
-  },
-];
+const data = [1, 2, 3, 4, 5];
 
 function searchData(data) {
-  const today = new Date().toDateString();
-  const [{ id, date }] = data.filter((dataItem) => dataItem.date === today);
-  return id;
+  const today = new Date().getDay();
+  const [day] = data.filter((day) => day === today);
+  return day;
 }
 
 export default function Schedule() {
@@ -114,10 +29,10 @@ export default function Schedule() {
           {data.map((dataItem, index) => (
             <SwiperSlide key={index}>
               <LightGallery download={false} counter={false}>
-                <a href={`./img/Schedule/${dataItem.id}.jpg`}>
+                <a href={`./img/Schedule/${dataItem}.jpg`}>
                   <img
                     className={classes.scheduleSwiperImg}
-                    src={`./img/Schedule/${dataItem.id}.png`}
+                    src={`./img/Schedule/${dataItem}.png`}
                   />
                 </a>
               </LightGallery>
